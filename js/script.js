20624c8619779bb64ce971a9a1f4d7bc58133c86
RaindanceMaggiie <kasias608@gmail.com>
2019-12-03 19:18:28 +0100

    My first git commit

--- js/script.js
+++ js/script.js
@@ -1,38 +1,54 @@
-const comments = [
-  {
-    name: "Martin Luther King",
-    comment: "Amen to that"
-  }
-];
+// const comments = [
+//   {
+//     name: "Martin Luther King",
+//     comment: "Amen to that"
+//   }
+// ];
 
-const user = {
-  name: "Barack Obama"
-};
+// const user = {
+//   name: "Barack Obama"
+// };
 
-const addComment = event => {
-  const commentInput = document.getElementById("comment-input");
-  const comment = commentInput.value;
-  const target = document.getElementById("comments");
+// const addComment = event => {
+//   const commentInput = document.getElementById("comment-input");
+//   const comment = commentInput.value;
+//   const target = document.getElementById("comments");
 
-  if (comment.length > 0) {
-    comments.push({ name: user.name, comment });
+//   if (comment.length > 0) {
+//     comments.push({ name: user.name, comment });
 
-    const newComments = comments
-      .map(
-        comment => `<div class="single-comment">
-  <p class="comment-text">${comment.comment}</p>
-  </div>`
-      )
-      .join(" ");
+//     const newComments = comments
+//       .map(
+//         comment => `<div class="single-comment>
+//   <p class="comment-text">${comment.comment}</p>
+//   </div>`
+//       )
+//       .join(" ");
 
-    target.innerHTML = newComments;
+//     target.innerHTML = newComments;
 
-    comment.value = "";
-    window.scrollTo(0, document.body.scrollHeight);
+//     comment.value = "";
+//     window.scrollTo(0, document.body.scrollHeight);
+//     event.preventDefault();
+//   }
+// };
+
+// document
+//   .getElementById("comment-button")
+//   .addEventListener("click", event => addComment(event));
+
+const newArray = ["komentarz", "inny komentarz", "głupotki"];
+const newComments = document.getElementById("newComments");
+  newArray.forEach( comment => {newComments.innerText += comment})
+  
+const newFunction = event => {
     event.preventDefault();
-  }
-};
+    const commentInput = document.getElementById("comment-input")
+    let commentValue = commentInput.value
+    newComments.innerText += commentValue
+ }
 
-document
-  .getElementById("comment-button")
-  .addEventListener("click", event => addComment(event));
+  
+document.getElementById("comment-button")
+  .addEventListener("click", (event) => newFunction(event));
+
