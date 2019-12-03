1eb2618585c01920d53a8a116cc332fb1c92f7e0
AdamZaczek <adamzaczekul@gmail.com>
2019-12-03 15:32:01 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const comments = [
+    {
+      name: "Martin Luther King",
+      comment: "Amen to that"
+    }
+  ];
+  
+  const user = {
+    name: "Barack Obama"
+  };
+  
+  const addComment = event => {
+    const commentInput = document.getElementById("comment-input");
+    const comment = commentInput.value;
+    const target = document.getElementById("comments");
+  
+    if (comment.length > 0) {
+      comments.push({ name: user.name, comment });
+  
+      const newComments = comments
+        .map(
+          comment => `<div class="single-comment">
+  <p class="date">${new Date().toISOString().slice(0, 10)}:</p>
+  <p class="comment-text">${comment.comment}</p>
+  </div>`
+        )
+        .join(" ");
+  
+      target.innerHTML = newComments;
+  
+      comment.value = "";
+      window.scrollTo(0, document.body.scrollHeight);
+      event.preventDefault();
+    }
+  };
+  
+  document
+    .getElementById("comment-button")
+    .addEventListener("click", event => addComment(event));
+  
