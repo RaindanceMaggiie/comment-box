4c281f3ad87e626819a5738d821735eab8bb5a7e
AdamZaczek <adamzaczekul@gmail.com>
2019-12-03 15:39:45 +0100

    Prettier fixes

--- js/script.js
+++ js/script.js
@@ -1,39 +1,38 @@
 const comments = [
-    {
-      name: "Martin Luther King",
-      comment: "Amen to that"
-    }
-  ];
-  
-  const user = {
-    name: "Barack Obama"
-  };
-  
-  const addComment = event => {
-    const commentInput = document.getElementById("comment-input");
-    const comment = commentInput.value;
-    const target = document.getElementById("comments");
-  
-    if (comment.length > 0) {
-      comments.push({ name: user.name, comment });
-  
-      const newComments = comments
-        .map(
-          comment => `<div class="single-comment">
+  {
+    name: "Martin Luther King",
+    comment: "Amen to that"
+  }
+];
+
+const user = {
+  name: "Barack Obama"
+};
+
+const addComment = event => {
+  const commentInput = document.getElementById("comment-input");
+  const comment = commentInput.value;
+  const target = document.getElementById("comments");
+
+  if (comment.length > 0) {
+    comments.push({ name: user.name, comment });
+
+    const newComments = comments
+      .map(
+        comment => `<div class="single-comment">
   <p class="comment-text">${comment.comment}</p>
   </div>`
-        )
-        .join(" ");
-  
-      target.innerHTML = newComments;
-  
-      comment.value = "";
-      window.scrollTo(0, document.body.scrollHeight);
-      event.preventDefault();
-    }
-  };
-  
-  document
-    .getElementById("comment-button")
-    .addEventListener("click", event => addComment(event));
-  
+      )
+      .join(" ");
+
+    target.innerHTML = newComments;
+
+    comment.value = "";
+    window.scrollTo(0, document.body.scrollHeight);
+    event.preventDefault();
+  }
+};
+
+document
+  .getElementById("comment-button")
+  .addEventListener("click", event => addComment(event));
